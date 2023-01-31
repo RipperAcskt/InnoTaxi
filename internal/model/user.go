44caab75ff80db3327b55c721faf46def3a209a0
Ripper <7309333@gmail.com>
2023-01-31 19:11:21 +0300

    refactoring

--- internal/model/user.go
+++ internal/model/user.go
@@ -6,7 +6,6 @@ const (
 )
 
 type User struct {
-	UserID      uint64
 	ID          uint64  `json:"-"`
 	Name        *string `json:"name"`  `json:"name"`
 	PhoneNumber *string `json:"phone_number"`  `json:"phone_number"`
