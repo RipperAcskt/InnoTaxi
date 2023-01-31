d8c10c8a8eaefa7206b6105a76422187cbeb1410
Ripper <7309333@gmail.com>
2023-01-31 14:44:03 +0300

    refactoring

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,10 +1,15 @@
 package model
 
+const (
+	StatusCreated string = "created"
+	StatusDeleted string = "deleted"
+)
+
 type User struct {
 	UserID      uint64
 	Name        *string `json:"name"`
 	PhoneNumber *string `json:"phone_number"`
 	Email       *string `json:"email"`
 	Raiting     float64
-	Status      int
+	Status      string
 }
