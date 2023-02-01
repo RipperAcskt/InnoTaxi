ab960d8b1cf3e42077e642df9dc83c6573838799
Ripper <7309333@gmail.com>
2023-02-01 12:28:13 +0300

    refactoring

--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -32,12 +32,12 @@ func (h *Handler) InitRouters() *gin.Engine {
 	auth := users.Group("/auth")
 	auth.POST("sing-up", h.singUp)
 	auth.POST("sing-in", h.singIn)
-	auth.POST("refresh", h.Refresh)
+	auth.GET("refresh", h.Refresh)
+	auth.GET("logout", h.VerifyToken(), h.Logout)
 
 	users.GET("/profile/:id", VerifyToken(h.cfg), h.GetProfile)
 	users.PUT("/profile/:id", VerifyToken(h.cfg), h.UpdateProfile)
 	users.DELETE("/:id", VerifyToken(h.cfg), h.DeleteUser)
-	auth.GET("refresh", h.Refresh)
-	auth.GET("logout", h.VerifyToken(), h.Logout)
+
 	return router
 }
